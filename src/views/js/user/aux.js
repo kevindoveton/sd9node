ea114d830e4ca1205285b624c74d8dfeaff07b00
kevindoveton <kevindoveton@me.com>
2017-01-23 20:25:18 +1030

    added comment explaining last commit

--- src/views/js/user/aux.js
+++ src/views/js/user/aux.js
@@ -40,7 +40,7 @@ $(function() {
 		}
 	});
 
-	socket.on('volume/aux/'+auxnumber, function (data) {
+	socket.on('volume/aux/'+auxnumber, function (data) { // +auxnumber may not be needed
 		data = JSON.parse(data);
 
 		if (data["a"] == auxnumber) {
