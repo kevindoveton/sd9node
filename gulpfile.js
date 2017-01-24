fafb4b85bc9d30d0e5da28ccae72485003ce93e9
kevindoveton <kevindoveton@me.com>
2017-01-24 12:38:24 +1030

    changed gulp and reorganised directories to split server and client code

--- gulpfile.js
+++ gulpfile.js
@@ -14,31 +14,70 @@ var gulp = require('gulp'),
 /*
  * Directories here
  */
-var paths = {
-  public: './src/static/',
-  sass: './src/views/sass/',
-  js: './src/static/js/',
-  css: './src/static/css/'
+// var client_paths = {
+// 	base_dist: './src/client_static/',
+// 	css: this.base_dist + 'css/',
+// 	js_dist: this.base_dist + 'js/',
+// 	html_dist: this.base_dist + 'html/',
+//
+// 	base_build: './src/client_views/',
+// 	sass: this.base_build + 'sass/',
+// 	js_build: this.base_build + 'js/',
+// 	pug: this.base_build + 'pug/'
+// };
+
+var client_paths = new function() {
+	this.base_dist = './src/client_static/';
+	this.css = this.base_dist + 'css/';
+	this.js_dist = this.base_dist + 'js/';
+	this.html_dist = this.base_dist + 'html/';
+
+	this.base_build = './src/client_views/';
+	this.sass = this.base_build + 'sass/';
+	this.js_build = this.base_build + 'js/';
+	this.pug = this.base_build + 'pug/';
+};
+
+var server_paths = new function() {
+	this.base_dist = './src/server_static/';
+	this.css = this.base_dist + 'css/';
+	this.js_dist = this.base_dist + 'js/';
+	this.html_dist = this.base_dist + 'html/';
+
+	this.base_build = './src/server_views/';
+	this.sass = this.base_build + 'sass/';
+	this.js_build = this.base_build + 'js/';
+	this.pug = this.base_build + 'pug/';
 };
 
 /**
  * Compile .pug files and pass in data from json file
  * matching file name. index.pug - index.pug.json
  */
-gulp.task('pug', function () {
+gulp.task('client_pug', function () {
 	pump([
-		gulp.src('./src/views/*.pug'),
+		gulp.src(client_paths.pug + '*.pug'),
 		pug(),
-		gulp.dest(paths.public+'html/')
+		gulp.dest(client_paths.html_dist)
 	]);
 	return;
 });
 
-gulp.task('js', function() {
-	console.log('js');
+gulp.task('server_pug', function () {
+	pump([
+		gulp.src(server_paths.pug + '*.pug'),
+		pug(),
+		gulp.dest(server_paths.html_dist)
+	]);
+	return;
+});
+
+
+// JS
+gulp.task('client_js', function() {
 	// user
 	pump([
-		gulp.src('./src/views/js/user/*.js'),
+		gulp.src(client_paths.js_build + 'user/*.js'),
 		concat('dist.js'),
 		uglify({
 			mangle: false,
@@ -47,7 +86,7 @@ gulp.task('js', function() {
 				beautify: true
 			}
 		}),
-		gulp.dest(paths.js)
+		gulp.dest(client_paths.js_dist)
 	], function(e) {
 		if (e !== undefined)
 		{
@@ -57,7 +96,7 @@ gulp.task('js', function() {
 
 	// vendor
 	pump([
-		gulp.src('./src/views/js/vendor/*.js'),
+		gulp.src(client_paths.js_build + 'vendor/*.js'),
 		concat('vendor.js'),
 		uglify({
 			mangle: false,
@@ -66,7 +105,48 @@ gulp.task('js', function() {
 				beautify: false
 			}
 		}),
-		gulp.dest(paths.js)
+		gulp.dest(client_paths.js_dist)
+	], function(e) {
+		if (e !== undefined)
+		{
+			console.log(e);
+		}
+	});
+	return;
+});
+
+gulp.task('server_js', function() {
+	// user
+	pump([
+		gulp.src(server_paths.js_build + 'user/*.js'),
+		concat('dist.js'),
+		uglify({
+			mangle: false,
+			compress: false,
+			output: {
+				beautify: true
+			}
+		}),
+		gulp.dest(server_paths.js_dist)
+	], function(e) {
+		if (e !== undefined)
+		{
+			console.log(e);
+		}
+	});
+
+	// vendor
+	pump([
+		gulp.src(server_paths.js_build + 'vendor/*.js'),
+		concat('vendor.js'),
+		uglify({
+			mangle: false,
+			compress: false,
+			output: {
+				beautify: false
+			}
+		}),
+		gulp.dest(server_paths.js_dist)
 	], function(e) {
 		if (e !== undefined)
 		{
@@ -80,11 +160,11 @@ gulp.task('js', function() {
  * Compile .scss files into public css directory With autoprefixer no
  * need for vendor prefixes then live reload the browser.
  */
-gulp.task('sass', function () {
+gulp.task('client_sass', function () {
 	pump([
-		gulp.src(paths.sass + '*.sass'),
+		gulp.src(client_paths.sass + '*.sass'),
 		sass({
-			includePaths: [paths.sass],
+			includePaths: [client_paths.sass],
 			outputStyle: 'compressed'
 	  	}),
 		prefix(
@@ -98,7 +178,35 @@ gulp.task('sass', function () {
 				cascade: true
 	    	}
 		),
-		gulp.dest(paths.css)
+		gulp.dest(client_paths.css)
+	], function(e) {
+		if (e !== undefined)
+		{
+			console.log(e);
+		}
+	});
+	return;
+});
+
+gulp.task('server_sass', function () {
+	pump([
+		gulp.src(server_paths.sass + '*.sass'),
+		sass({
+			includePaths: [server_paths.sass],
+			outputStyle: 'compressed'
+	  	}),
+		prefix(
+			[
+				'last 15 versions',
+				'> 1%',
+				'ie 8',
+				'ie 7'
+			],
+			{
+				cascade: true
+	    	}
+		),
+		gulp.dest(server_paths.css)
 	], function(e) {
 		if (e !== undefined)
 		{
@@ -112,18 +220,25 @@ gulp.task('sass', function () {
  * Watch scss files for changes & recompile
  * Watch .pug files run pug-rebuild then reload BrowserSync
  */
-gulp.task('watch', function () {
-  gulp.watch(paths.sass + '**/*.sass', ['sass']);
-  gulp.watch('./src/views/*.pug', ['pug']);
-  gulp.watch('./src/views/js/**/*.js', ['js', 'js']);
+gulp.task('client_watch', function () {
+	gulp.watch(client_paths.sass + '**/*.sass', ['client_sass']);
+	gulp.watch(client_paths.pug + '*.pug', ['client_pug']);
+	gulp.watch(client_paths.js_build + '**/*.js', ['client_js']);
+});
+
+gulp.task('server_watch', function () {
+	gulp.watch(server_paths.sass + '**/*.sass', ['server_sass']);
+	gulp.watch(server_paths.pug + '*.pug', ['server_pug']);
+	gulp.watch(server_paths.js_build + '**/*.js', ['server_js']);
 });
 
 // Build task compile sass and pug.
-gulp.task('build', ['sass', 'pug', 'js']);
+gulp.task('client_build', ['client_sass', 'client_pug', 'client_js']);
+gulp.task('server_build', ['server_sass', 'server_pug', 'server_js']);
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync then watch
  * files for changes
  */
-gulp.task('default', ['build','watch']);
+gulp.task('default', ['client_build', 'client_watch', 'server_build', 'server_watch']);
