051610d3e6aa3eb037ebf47680a4041f9355ea6d
kevindoveton <kevindoveton@me.com>
2017-01-25 10:55:48 +1030

    added button logic

---
+++ src/server_views/js/user/index.js
@@ -0,0 +1,25 @@
+var openSettings = function() {
+	if ($("div.settings").css("opacity") == 1) {
+		$("div.settings").css({opacity: 0});
+		setTimeout(function() {
+			$("div.settings").css({'z-index': -1});
+		}, 1000)
+	}
+	else {
+		$("div.settings").css({'z-index': 2, opacity: 1});
+	}
+}
+
+var connect = function() {
+	alert('connect!');
+}
+
+$(function() {
+	$(".openSettings").on({
+		'click' : openSettings
+	});
+
+	$("#connectBtn").on({
+		'click': connect
+	});
+});
