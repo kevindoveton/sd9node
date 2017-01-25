04c8a60e9678cba0a1b7d0232424a37e1361ec51
kevindoveton <kevindoveton@me.com>
2017-01-25 20:40:20 +1030

    added electron rebuild for osc/electron support

--- src/main.js
+++ src/main.js
@@ -57,4 +57,4 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 // vendor
-// const server = require('./app')();
+var server = require('./app')();
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,19 @@
 module.exports = function(app, express) {
 	// static files
-	app.use('/static', express.static('static'));
+	app.use('/static', express.static('client_static'));
 
 	// aux home page
 	app.get('/', function (req, res) {
-		res.sendFile('static/html/index.html' , { root : __dirname});
+		res.sendFile('client_static/html/index.html' , { root : __dirname});
 	});
 
 	// all aux
 	app.get('/aux/:id', function (req, res) {
-		res.sendFile('static/html/aux.html' , { root : __dirname});
+		res.sendFile('client_static/html/aux.html' , { root : __dirname});
 	});
 
 	// redirect to aux home
 	app.get('/aux', function (req, res) {
-		res.sendFile('static/html/index.html' , { root : __dirname});
+		res.sendFile('client_static/html/index.html' , { root : __dirname});
 	});
 }
